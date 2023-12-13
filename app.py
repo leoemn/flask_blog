42192e7e53a88f218aaa02f7785aaf78efd5b725
leoemn <sagarjoshi1013@gmail.com>
2023-12-13 18:32:45 +0000

    Added route to create post,implemented Database

--- app.py
+++ app.py
@@ -1,31 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from  flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
 @app.route('/')
 def home():
-    posts = [
-        {'id': '1', 'title': 'First Post', 'content': 'Content of the first post'},
-        {'id':'2', 'title': 'Second Post', 'content': 'Content of the second post'},
-        {'id': '3', 'title': 'Third Post', 'content': 'Contetn of the third post'},
-    ]
+
+    posts = Post.query.all()
+    print(posts)
 
     return render_template('index.html', message = 'This is message', posts = posts)
 
-@app.route('/post/<post_id>')
+@app.route('/post/<int:post_id>')
 def post(post_id):
-    posts = [
-        {'id': '1', 'title': 'First Post', 'content': 'Content of the first post'},
-        {'id':'2', 'title': 'Second Post', 'content': 'Content of the second post'},
-        {'id': '3', 'title': 'Third Post', 'content': 'Contetn of the third post'},
-    ]
-
-    selected_post = next((post for  post in posts if post['id'] == post_id), None)
+    
+    selected_post = Post.query.get(post_id)
 
     if not selected_post:
         return render_template('404.html')
     
     return render_template('post.html', post = selected_post)
 
+@app.route('/create_post', methods=['GET', 'POST'])
+def create_post():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        new_post = Post()
+        new_post.title = title
+        new_post.content = content
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    
+    return render_template('create_post.html')
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug = True)
