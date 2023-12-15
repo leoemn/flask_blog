8e13dddfec8d8c6bb1eeb73f5f4347951a71f184
leoemn <sagarjoshi1013@gmail.com>
2023-12-15 19:56:18 +0000

    added some commnets and routes

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
-from flask import Flask, redirect, render_template, request, url_for
+from enum import unique
+from flask import Flask, redirect, render_template, url_for, request, flash
 from  flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import LoginManager, UserMixin, LoginManger, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'AbCDefGHIjklmnOPqRsTuVwxyZ'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+# Define the Post model
 class Post(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+# Define the User model
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    posts = db.relationship('Post',backref = 'author',lazy=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.string(60), nullable=False)
+    posts = db.relationship('Post',backref='author',lazy=True)
 
+# User loader function for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Route for the home page
 @app.route('/')
 def home():
 
     posts = Post.query.all() 
 
     return render_template('index.html', message = 'This is message', posts = posts)
-
+# Route for displaying an individual post
 @app.route('/post/<int:post_id>')
 def post(post_id):
     
@@ -38,7 +50,9 @@ def post(post_id):
     
     return render_template('post.html', post = selected_post)
 
+# Route for creating a new post 
 @app.route('/create_post', methods=['GET', 'POST'])
+@login_required
 def create_post():
 
     if request.method == 'POST':
@@ -54,6 +68,21 @@ def create_post():
         return redirect(url_for('home'))
     
     return render_template('create_post.html')
+
+# Route for handling user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            flash('Successful!', 'success')
+            return redirect(url_for('home'))
+        flash('Login failed, Check your username and password.', 'danger')
+        return render_template('login.html')
+
 
 if __name__ == '__main__':
     with app.app_context():
